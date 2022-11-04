13f727951bcad967abaa84a7c58802f093cc6fa6
Michael George <michael.g.george@gmail.com>
2022-11-05 00:48:51 +0530

    Add make_tag_call to script
    New file:   portswigger/xss/reflected/svg_allowed/script.py

---
+++ portswigger/xss/reflected/svg_allowed/script.py
@@ -0,0 +1,12 @@
+from ratelimit import limits, RateLimitException, sleep_and_retry
+from pathlib import Path
+from requests import get
+
+ONE_MINUTE = 60
+MAX_CALLS_PER_MINUTE = 120
+url = 'https://INSTANCE-ID.web-security-academy.net/'
+
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
+def make_tag_call(url, tag):
+    pass
