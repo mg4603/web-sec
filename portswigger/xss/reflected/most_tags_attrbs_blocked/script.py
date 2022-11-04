ac356c5f4edcd0add37b9fe8deef51f3849db268
Michael George <michael.g.george@gmail.com>
2022-11-04 07:12:30 +0530

    Modify make_tag_call in most_tags_attrbs_blocked
    Added angle brackets to tags.

--- portswigger/xss/reflected/most_tags_attrbs_blocked/script.py
+++ portswigger/xss/reflected/most_tags_attrbs_blocked/script.py
@@ -2,7 +2,7 @@ from requests import get
 from ratelimit import limits, RetryLimitException, sleep_and_retry
 from pathlib import Path
 
-url = 'https://INSTANCE-ID.web-security-academy.net/?search='
+url = 'https://INSTANCE-ID.web-security-academy.net/?search=<'
 
 ONE_MINUTE = 60
 MAX_CALLS_PER_MINUTE = 120
@@ -10,7 +10,7 @@ MAX_CALLS_PER_MINUTE = 120
 @sleep_and_retry
 @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
 def make_tag_call(url, tag):
-    return get(url + tag)
+    return get(f"{url}{tag}>")
 
 def tag_enum(url, tag_file_path):
     with Path(tag_file_path).open('r') as file:
@@ -20,4 +20,3 @@ def tag_enum(url, tag_file_path):
                 return tag
 
     return "NOT FOUND"
-
