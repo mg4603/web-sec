e48c3e7b7b34bd7a143923bdbb022e41495ef8e7
Michael George <michael.g.george@gmail.com>
2022-11-06 02:06:46 +0530

    Add make_user_call to script
    New file: portswigger/auth/user_enum_subtly_diff_res/script.py

---
+++ portswigger/auth/user_enum_subtly_diff_res/script.py
@@ -0,0 +1,20 @@
+from ratelimit import limits, RateLimitException, sleep_and_retry
+from requests import post
+from pathlib import Path
+
+ONE_MINUTE = 60
+MAX_CALLS_PER_MINUTE = 120
+
+url = ''
+headers = {
+
+}
+cookies = {
+
+}
+
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
+def make_user_call(url, user, headers, cookies):
+    pass
+
